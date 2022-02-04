a213fdf3f73398df1044997b316afa6191ba6a65
Samuele Francesconi <sam@MBP-di-Samuele.lan>
2022-02-04 12:23:35 +0100

    materials added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from '../assets/logo.jpg';
+import fourSeason from '../assets/fourSeason.png'
+import belmond from '../assets/belmond.png';
+import como from '../assets/como.png';
+import aman from '../assets/aman.png'
 import '../styles/scss/_Banner.scss';
 
 export default function Banner(props) {
@@ -7,9 +10,10 @@ export default function Banner(props) {
         <div className='banner'>
             <h6>I NOSTRI PARTNER</h6>
             <div className="partner__logos">
-                <img src={logo} alt='logo'/>
-                <img src={logo} alt='logo'/>
-                <img src={logo} alt='logo'/> 
+                <img src={fourSeason} alt='logo'/>
+                <img src={belmond} alt='logo'/>
+                <img src={como} alt='logo'/> 
+                <img src={aman} alt='logo'/>
             </div>
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/scss/_Header.scss';
-import logo from '../assets/logo.jpg';
+import logoTr from '../assets/logotr.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header(props) {
     return (
     <div className='header'>
-        <img src={logo} alt='logo'/>
+        <img src={logoTr} alt='logo'/>
         <FontAwesomeIcon icon={faBars}/>
     </div>
     );

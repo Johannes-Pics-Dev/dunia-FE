4846a7c75c57242ff33dd14f3902b97f43cfbccc
filippojohannes <82021710+filippojohannes@users.noreply.github.com>
2022-04-11 16:24:59 +0200

    legal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,14 @@ import { Container,Row,Col } from 'react-bootstrap';
 export default function Footer(props) {
     return (
         <Container>
-        <Row className="justify-content-center text-center">
+        <Row className="justify-content-center flex-column text-center">
+          <Col md="auto">
+            <p class="legal_footer">P.Iva - IT12071420967 Rea - MI - 2638514 Capitale sociale  10.000,00</p>
+          </Col>
           <Col md="auto">
             <img src={logo} className="img-fluid img-footer" />
           </Col>
+          
         </Row>
         </Container>
     );

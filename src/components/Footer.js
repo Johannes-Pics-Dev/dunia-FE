f7f3006cc67bb9f80c97629a5d56d56982b65bf0
filippojohannes <82021710+filippojohannes@users.noreply.github.com>
2022-05-04 14:31:46 +0200

    Privacy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.jpg';
 import { Container,Row,Col } from 'react-bootstrap';
+import Pdf from '../privacy/privacy_dunia.pdf';
 
 export default function Footer(props) {
     return (
@@ -8,6 +9,9 @@ export default function Footer(props) {
         <Row className="justify-content-center flex-column text-center">
           <Col md="auto">
             <p class="legal_footer">P.Iva - IT12071420967 Rea - MI - 2638514 Capitale sociale  10.000,00</p>
+          </Col>
+          <Col md="auto">
+            <a class="legal_footer_link" href={Pdf} target="_blank">Privacy Policy</a>
           </Col>
           <Col md="auto">
             <img src={logo} className="img-fluid img-footer" />
